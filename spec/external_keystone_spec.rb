7133783b0ecbbdcd4e2408acf8d5f45702fb9a61
zshuo <shuozhang@cn.ibm.com>
2016-03-04 15:24:03 +0800

    Update external_keystone_spec.rb

--- spec/external_keystone_spec.rb
+++ spec/external_keystone_spec.rb
@@ -1,16 +1,23 @@
 # encoding: UTF-8
-# =================================================================
-# Licensed Materials - Property of IBM
-#
-# Copyright 2015, 2016 IBM Corp.
-#
-# US Government Users Restricted Rights - Use, duplication or
-# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-# =================================================================
 #
 # Cookbook Name:: openstack-zvm-external-keystone
 # Spec:: external_keystone_spec
 #
+# Copyright 2015, 2016 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 require_relative 'spec_helper'
 
 describe 'openstack-zvm-external-keystone::external_keystone' do
